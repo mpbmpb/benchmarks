15e45a5a4f2b46e34b074fc617b6b167413e3195
mpbmpb <markkeyster@gmail.com>
2022-01-10 22:11:31 +0100

    added benchmarks logging to void to see allocations

--- Benchmarks/LoggingTests.cs
+++ Benchmarks/LoggingTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Serilog;
 using Serilog.Core;
+using Serilog.Events;
 
 namespace Benchmarks;
 
@@ -17,11 +19,16 @@ public class LoggingTests
 
     private readonly ILogger<LoggingTests> _logger;
     private readonly ILoggerAdapter<LoggingTests> _loggerAdapter;
+    private readonly NullLogger<LoggingTests> _nullLogger = new();
+    private readonly ILoggerAdapter<LoggingTests> _nullAdapter;
+    private StringWriter _messages = new();
 
     private readonly Logger log = new LoggerConfiguration()
         .MinimumLevel.Warning()
         .WriteTo.Console()
         .CreateLogger();
+
+    private readonly Logger nullLog;
     
     
 
@@ -29,6 +36,11 @@ public class LoggingTests
     {
         _logger = new Logger<LoggingTests>(_loggerFactory);
         _loggerAdapter = new LoggerAdapter<LoggingTests>(_logger);
+        _nullAdapter = new LoggerAdapter<LoggingTests>(_nullLogger);
+        nullLog = new LoggerConfiguration()
+            .MinimumLevel.Warning()
+            .WriteTo.TextWriter(_messages)
+            .CreateLogger();
     }
 
     [Benchmark]
@@ -73,6 +85,24 @@ public class LoggingTests
         log.Information(MessageWithParameters, 42, 101);
     }
 
+    [Benchmark]
+    public void Log_to_void_with_parameters()
+    {
+        _nullLogger.LogWarning(MessageWithParameters, 42, 101);
+    }
+
+    [Benchmark]
+    public void Log_to_void_using_Adapter_with_parameters()
+    {
+        _nullAdapter.LogWarning(MessageWithParameters, 42, 101);
+    }
+
+    [Benchmark]
+    public void Log_to_void_using_Serilog_with_parameters()
+    {
+        nullLog.Information(MessageWithParameters, 42, 101);
+    }
+    
 }
 
 
@@ -83,6 +113,13 @@ public interface ILoggerAdapter<T>
     void LogInformation<T0>(string message, T0 arg0);
     
     void LogInformation<T0, T1>(string message, T0 arg0, T1 arg1);
+    
+    void LogWarning(string message);
+    
+    void LogWarning<T0>(string message, T0 arg0);
+    
+    void LogWarning<T0, T1>(string message, T0 arg0, T1 arg1);
+    
 }
 
 public class LoggerAdapter<T> : ILoggerAdapter<T>
@@ -116,5 +153,29 @@ public class LoggerAdapter<T> : ILoggerAdapter<T>
         {
             _logger.LogInformation(message, arg0, arg1);
         }
+    }
+
+    public void LogWarning(string message)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogWarning(message);
+        }
+    }
+
+    public void LogWarning<T0>(string message, T0 arg0)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogWarning(message, arg0);
+        }   
+    }
+
+    public void LogWarning<T0, T1>(string message, T0 arg0, T1 arg1)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogWarning(message, arg0, arg1);
+        }    
     }
 }
